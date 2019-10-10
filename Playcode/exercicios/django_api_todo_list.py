21e19e40f212fd96da3d4f298581f5e3df495fe4
André Girol <andregirol@gmail.com>
2019-10-10 17:13:16 -0300

    Adicionando mais exercícios

---
+++ Playcode/exercicios/django_api_todo_list.py
@@ -0,0 +1,11 @@
+# 4 - Django - TODO lists
+"""
+# Django - Todo APP
+- Create a django API to manage to-do lists.
+- Requirements:
+    - Create a model to represent the tasks. Each task must have at least a title, description and status. Feel free to add other relevant fields.
+    - Create a restful API to manage tasks (create, delete, update and read)
+    - Write tests
+https://medium.com/fbdevclagos/how-to-build-a-todo-app-with-django-17afdc4a8f8c
+
+"""
