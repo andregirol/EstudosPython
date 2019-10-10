6700c132816110aae2c6c930cc7c86fe36cbebcf
André Girol <andregirol@gmail.com>
2019-10-10 14:26:21 -0300

    Adicionado exercicio com listas

---
+++ Playcode/exercicios/lists.py
@@ -0,0 +1,17 @@
+# 1 - Lists
+
+# given list
+
+numbers = [1, 2, 3, 4]
+
+# Task 1 - Create a new list, calculating the square number (x * x) for each item in the numbers lit
+# Expected output:
+# [1, 4, 9, 16]
+
+
+# Task 2 - Filter all the even numbers in the list
+# Expected output: [2, 4]
+
+
+# Task 3 - Sum all the odd numbers in the list
+# Expected output: 4
